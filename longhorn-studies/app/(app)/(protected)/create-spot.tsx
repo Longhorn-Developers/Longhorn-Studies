a83b7a0ef34a2b157bdb593e71a4a2d04a891c94
Astrol99 <dngames2006@gmail.com>
2025-04-22 01:51:33 -0500

    feat: added database types with supazod

--- longhorn-studies/app/(app)/(protected)/create-spot.tsx
+++ longhorn-studies/app/(app)/(protected)/create-spot.tsx
@@ -1,7 +1,19 @@
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
+
+import { Container } from '~/components/Container';
+
+import { PublicSpotsInsertSchema } from '~/types/schemas_infer';
 
 const createSpot = () => {
-  return <View className="flex-1 items-center justify-center"></View>;
+  return (
+    <Container>
+      <View className="h-full justify-between">
+        <View className="gap-3">
+          <Text>asdasdasd</Text>
+        </View>
+      </View>
+    </Container>
+  );
 };
 
 export default createSpot;
