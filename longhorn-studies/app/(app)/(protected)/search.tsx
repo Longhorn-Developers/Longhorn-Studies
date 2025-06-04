96da8f96b198b77543cf0225cef5a330d533ca87
Astrol99 <dngames2006@gmail.com>
2025-06-04 15:45:31 -0500

    feat: added trending tags and popular spots in search

--- longhorn-studies/app/(app)/(protected)/search.tsx
+++ longhorn-studies/app/(app)/(protected)/search.tsx
@@ -1,4 +1,5 @@
-import { View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { Text, View } from 'react-native';
 
 import { Container } from '~/components/Container';
 import Searchbox from '~/components/Searchbox';
@@ -7,9 +8,24 @@ import TagSelector from '~/components/TagSelector';
 const Search = () => {
   return (
     <Container>
-      <View className="gap-2">
+      <View className="gap-4">
         <Searchbox sharedTag="explore-search" />
-        <TagSelector />
+        {/* Trending Tags */}
+        <View className="flex">
+          <View className="mb-2 flex-row items-center gap-2">
+            <Ionicons name="trending-up" size={20} color="gray" />
+            <Text className="text-lg font-semibold">Trending Tags</Text>
+          </View>
+          <TagSelector />
+        </View>
+
+        {/* Trending Posts */}
+        <View className="flex">
+          <View className="mb-2 flex-row items-center gap-2">
+            <Ionicons name="stats-chart" size={20} color="gray" />
+            <Text className="text-lg font-semibold">Popular Spots</Text>
+          </View>
+        </View>
       </View>
     </Container>
   );
